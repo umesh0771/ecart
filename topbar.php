c03ca45453c84d10021e7a780b78cac347ec93a9
Dark._.beacon <137978480+umesh0771@users.noreply.github.com>
2025-02-06 16:35:56 +0530

    Add files via upload

---
+++ topbar.php
@@ -0,0 +1,27 @@
+<header class="topbar">
+    <div class="topbar-content">
+        <!-- Left Side: Site Title -->
+        <h1 class="site-title">Efashion</h1>
+
+        <!-- Center: Search Bar -->
+        <div class="search-bar">
+            <input 
+                type="text" 
+                placeholder="Search for products..." 
+                class="search-input">
+        </div>
+
+        <!-- Right Side: Cart and Account Icons -->
+        <div class="icons">
+            <!-- Cart Icon -->
+            <a href="#" class="icon-link">
+                <img src="images/cart.png" alt="Cart" class="icon">
+            </a>
+            
+            <!-- Account Icon -->
+            <a href="#" class="icon-link">
+                <img src="account-icon.png" alt="Account" class="icon">
+            </a>
+        </div>
+    </div>
+</header>
